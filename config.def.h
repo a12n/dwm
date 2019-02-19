5d4692b56cde8ab2fad056c0b51ea139834c1dcf
Anton Yabchinskiy <arn@bestmx.net>
2019-02-19 14:42:32 +0300

    Replace Iceweasel rule with Chromium rule

--- config.def.h
+++ config.def.h
@@ -57,7 +57,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "Iceweasel", NULL,      NULL,       1 << 2,       0,           -1 },
+	{ "Chromium", NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "Emacs",    NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
